f01cd82f1b62bb20c53c77939c2c6d2d88601e71
Adewumi Adebayo <glinkonline@gmail.com>
2019-10-11 09:03:29 +0100

    Added the security question, answer and the confirm pages

---
+++ src/components/confirmqa.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="h-100 bg-white">
+        <div class="w-100 min-h-25 fl">
+            <img src="@/assets/img/logo.png" class="db center h2" />
+        </div>
+        <article class="pa4 black-80">
+            <form action="sign-up_submit" method="get" accept-charset="utf-8">
+                <p>Confirm Question And Answers</p>
+
+                <h3 class="fw9">Q. WHAT IS YOUR MOTHER'S MAIDEN NAME?</h3>
+                <div>A. Click on the 'Save with Tessa' button</div>
+
+                <h3 class="fw9">Q. WHAT IS YOUR MOTHER'S MAIDEN NAME?</h3>
+                <div>A. Click on the 'Save with Tessa' button</div>
+
+                <h3 class="fw9">Q. WHAT IS YOUR MOTHER'S MAIDEN NAME?</h3>
+                <div>A. Click on the 'Save with Tessa' button</div>
+                
+                <div class="bg-light-gray pa1 dib br-button mt4 w-100">
+                    <div class="bg-black pa2 pv3 center br-button fw6 tracked white"> CONFIRM </div>
+                </div>
+            </form>
+        </article>
+    </section>
+</template>
